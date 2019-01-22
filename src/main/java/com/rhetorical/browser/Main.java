03390c3ec74cd6e9b4804e2d9526a971baa546b5
Mr_Rhetorical <caleb.brock@broshin.com>
2019-01-22 00:26:02 -0800

    Added Home button, Reload button, Forward, and Back Button
    - Removed unused Maven plugins.
    - Added icon.
    - Added basic logic to distinguish user input between a website and a search term.
    - Added method to open file.
    - Ensured that text input box now resizes with the browser.
    - Added Home button.
    - Added Reload button.
    - Added Back/Forward buttons.
    - Changed updating of the URL to be handled in a listener that waits for the location property of the WebEngine to update.
    - Added settings webpage. Right now it's just a placeholder, but I plan to put JSON data to the file system using this settings file, and load it from the java app.

--- src/main/java/com/rhetorical/browser/Main.java
+++ src/main/java/com/rhetorical/browser/Main.java
@@ -4,10 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
 
 public class Main extends Application {
 
@@ -22,6 +26,7 @@ public class Main extends Application {
 		stage.setTitle("Rhetorical's Browser");
 		stage.setScene(new Scene(root, 1250, 690));
 		stage.setFullScreen(false);
+		stage.getIcons().add(new Image("com/rhetorical/browser/image/icon.jpg"));
 		stage.show();
 	}
 }
--- src/main/java/com/rhetorical/browser/MainWindowController.java
+++ src/main/java/com/rhetorical/browser/MainWindowController.java
@@ -1,41 +1,122 @@
 package com.rhetorical.browser;
 
-import javafx.event.ActionEvent;
+import javafx.concurrent.Worker;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.swing.event.ChangeListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainWindowController implements Initializable {
 
-	private final String htLink = "http://";
+	private String homeAddress = "";
+	private String prevAddress = "";
+	private String nextAddress = "";
 
 	@FXML
-	TextField addressBar;
+	public TextField addressBar;
 
 	@FXML
-	WebView webView;
+	public Button backButton;
 
-	WebEngine webEngine;
+	@FXML
+	public Button forwardButton;
 
-	public void goToAddress(String address) {
+	@FXML
+	public Button homeButton;
+
+	@FXML
+	public Button reloadButton;
+
+	@FXML
+	public WebView webView;
+
+	@FXML
+	public HBox hBox;
+
+
+	private WebEngine webEngine;
+
+	private void updatePreviousAddress(String currentAddress) {
+		prevAddress = currentAddress;
+		if (backButton.isDisable()) {
+			backButton.setDisable(false);
+		}
+	}
+
+	private void updateNextAddress(String currentAddress) {
+		nextAddress = currentAddress;
+		if (( nextAddress.equals(webEngine.getLocation()) || nextAddress.equals("") || nextAddress.equals(prevAddress) ) && !forwardButton.isDisable()) {
+			forwardButton.setDisable(true);
+		} else {
+			forwardButton.setDisable(false);
+		}
+	}
+
+	public void goBack() {
+		String loc = webEngine.getLocation();
+		goToAddress(prevAddress);
+		updateNextAddress(loc);
+	}
+
+	public void goForward() {
+		goToAddress(nextAddress);
+		updateNextAddress("");
+	}
+
+	public void reload() {
+		webEngine.reload();
+	}
+
+	public void goHome() {
+		webEngine.load(homeAddress);
+	}
+
+	public void goToAddress(String input) {
+
+		updatePreviousAddress(webEngine.getLocation());
+		updateNextAddress("");
+
+		webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+			if (newValue == Worker.State.FAILED) {
+				goToAddress("https://duckduckgo.com/?q=" + input);
+			}
+		});
+
+		String address = input;
+
+		if (!address.toLowerCase().startsWith("http") && !address.toLowerCase().startsWith("www")) {
+			if (address.contains(".")) {
+				address = "http://" + address;
+			} else {
+				goToAddress("https://duckduckgo.com/?q=" + input);
+			}
+		}
+
 		webEngine.load(address);
+	}
 
-		addressBar.setText(address);
+	public void openFile(String address, String viewedAddress) {
+		webEngine.loadContent(address);
 	}
 
 	public void submitAddress() {
 		String address = addressBar.getText();
 
-		if (!address.startsWith(htLink))
-			address = htLink + address;
+		if (address.equalsIgnoreCase("browser::settings")) {
+			openFile("com/rhetorical/browser/web/settings/settings.html", "browser::settings");
+		}
 
 		goToAddress(address);
 	}
@@ -44,20 +125,28 @@ public class MainWindowController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		webEngine = webView.getEngine();
 
-		EventHandler<KeyEvent> keyEventEventHandler = event -> {
+		HBox.setHgrow(addressBar, Priority.ALWAYS);
+
+		addressBar.addEventHandler(KeyEvent.KEY_PRESSED,  (event) -> {
 			if (event.getEventType() == KeyEvent.KEY_PRESSED
-				&& event.getCode() == KeyCode.ENTER) {
+					&& event.getCode() == KeyCode.ENTER) {
 				submitAddress();
 			}
-		};
-
-		addressBar.addEventHandler(KeyEvent.KEY_PRESSED,  keyEventEventHandler);
+		});
 
 		//Fix stutter when first changing site
 		addressBar.getText();
 
-		webEngine.load(htLink + "start.duckduckgo.com");
+		String htLink = "http://";
+		homeAddress = htLink + "start.duckDuckGo.com";
+		goHome();
 
-		addressBar.setText(htLink + "www.duckduckgo.com");
+		webEngine.locationProperty().addListener((observable) -> {
+			updatePreviousAddress(webEngine.getLocation());
+			if (prevAddress.equals(nextAddress)) {
+				updateNextAddress("");
+			}
+			addressBar.setText(prevAddress);
+		});
 	}
 }
